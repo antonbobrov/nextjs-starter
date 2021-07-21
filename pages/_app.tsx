d7fca4c9b79f8a585310bf7aa3175a2318322250
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-07-21 13:12:14 +0300

    Add viewportSize helper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../src/styles/globals.scss';
 import type { AppProps } from 'next/app';
 
+import '../src/utils/set-css/viewportSize';
+
 function MyApp ({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
---
+++ src/utils/set-css/viewportSize.ts
@@ -0,0 +1,37 @@
+import app from '../../app';
+
+export default (function viewportSize () {
+    if (app) {
+        app.viewport.add('', () => {
+            set();
+        }, {
+            name: 'Viewport Size',
+        });
+        app.pageLoad.add('', () => {
+            set();
+        });
+        set();
+    }
+
+    function set () {
+        if (!app) {
+            return;
+        }
+        // viewport width
+        app.html.style.setProperty('--vw', `${getVW()}px`);
+        // viewport height
+        app.html.style.setProperty('--vh', `${getVH()}px`);
+    }
+
+    function getVW () {
+        return app ? app.viewport.width / 100 : 0;
+    }
+    function getVH () {
+        return app ? app.viewport.height / 100 : 0;
+    }
+
+    return {
+        vw: () => getVW(),
+        vh: () => getVH(),
+    };
+}());
