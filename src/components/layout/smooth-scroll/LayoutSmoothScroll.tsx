d4eebd92d4b8ed9f3c3a862cd6ac013be3356e4b
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-08-30 10:48:37 +0300

    Add smooth scroll

---
+++ src/components/layout/smooth-scroll/LayoutSmoothScroll.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef } from 'react';
+import { SmoothScroll } from 'vevet';
+import app, { appSettings } from '../../../app';
+
+interface Data {
+    children: JSX.Element;
+}
+
+const LayoutSmoothScroll = ({
+    children,
+}: Data) => {
+    // smooth scroll outer
+    const outer = useRef<HTMLDivElement>(null);
+
+    // create smooth scrolling
+    useEffect(() => {
+        // only if available
+        if (!outer.current || !appSettings.useSmoothScroll) {
+            return () => {};
+        }
+
+        // smooth scroll
+        const scroll = new SmoothScroll({
+            selectors: {
+                outer: outer.current,
+            },
+            overscroll: false,
+        });
+        if (app) {
+            app.onPageCreated().then((page) => {
+                page.smoothScroll = scroll;
+            });
+        }
+
+        return () => {
+            scroll.destroy();
+        };
+    }, [outer]);
+
+    return (
+        <div
+            className="v-smooth-scroll"
+            ref={outer}
+        >
+            <div className="v-smooth-scroll__container">
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default LayoutSmoothScroll;
--- src/templates/home/Home.tsx
+++ src/templates/home/Home.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link';
+import { useEffect } from 'react';
 import PageDesc from '../../components/layout/page-desc/PageDesc';
 import getH1 from '../../utils/document/getH1';
 import styles from './Home.module.scss';
 import { TemplateBaseData } from '../_base/types';
+import AppPage from '../../app/AppPage';
+import LayoutSmoothScroll from '../../components/layout/smooth-scroll/LayoutSmoothScroll';
 
 
 
@@ -12,24 +15,37 @@ export interface HomeTemplateData extends TemplateBaseData {
 
 
 
-export const HomeTemplate = (
+const HomeTemplate = (
     prop: HomeTemplateData,
-) => (
-    <div className="page-content">
-        <div className={styles.home}>
+) => {
+    // create page instance
+    useEffect(() => {
+        const page = new AppPage({
+            name: prop.template,
+        });
+        page.create();
+    });
 
-            <div className="wrap">
-                <h1>
-                    {getH1(prop)}
-                </h1>
-                <PageDesc {...prop} />
-                <br />
-                <br />
-                <Link href="/not-existing-page">Test link</Link>
+    // render the template
+    return (
+        <LayoutSmoothScroll>
+            <div className={styles.home}>
+                <div className="page-content">
+
+                    <div className="wrap">
+                        <h1>
+                            {getH1(prop)}
+                        </h1>
+                        <PageDesc {...prop} />
+                        <br />
+                        <br />
+                        <Link href="/not-existing-page">Test link</Link>
+                    </div>
+
+                </div>
             </div>
-
-        </div>
-    </div>
-);
+        </LayoutSmoothScroll>
+    );
+};
 
 export default HomeTemplate;
