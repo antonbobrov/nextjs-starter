98583aab73af8a2c90709b69178d54e064dee322
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-08-30 09:32:50 +0300

    Update App

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,39 @@
 import { Application } from 'vevet';
+import AppPage from './app/AppPage';
 import isBrowser from './utils/browser/isBrowser';
 
-const app = isBrowser ? new Application() : false;
+const app = isBrowser ? new Application<AppPage>() : false;
 export default app;
+
+// App Settings
+const useSmoothScroll = !(!!app && app.isMobile);
+const useWindowScroll = !useSmoothScroll;
+export const appSettings = {
+    useSmoothScroll,
+    useWindowScroll,
+};
+
+// fix scroll
+if (appSettings.useWindowScroll && isBrowser) {
+    window.history.scrollRestoration = 'manual';
+}
+
+// scroll to top on resize (mobile bug)
+if (!!app && !useWindowScroll) {
+    app.viewport.add('', () => {
+        document.documentElement.scrollTop = 0;
+        document.body.scrollTop = 0;
+    }, {
+        timeout: 650,
+        name: 'iOS scroll resize bug',
+    });
+}
+
+// set scroll classes
+if (app) {
+    if (useWindowScroll) {
+        app.html.classList.add('use-native-scroll');
+    } else if (useSmoothScroll) {
+        app.html.classList.add('use-smooth-scroll');
+    }
+}
---
+++ src/app/AppPage.ts
@@ -0,0 +1,12 @@
+import { Page, SmoothScroll } from 'vevet';
+
+export default class AppPage extends Page {
+    // smooth scrolling
+    protected _smoothScroll?: SmoothScroll;
+    get smoothScroll () {
+        return this._smoothScroll;
+    }
+    set smoothScroll (val: SmoothScroll | undefined) {
+        this._smoothScroll = val;
+    }
+}
