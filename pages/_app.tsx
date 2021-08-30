92ab65aa3d013f8836c4caf1e1df6eb7bbf606e7
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-08-30 09:32:24 +0300

    Update app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../src/styles/globals.scss';
 import type { AppProps } from 'next/app';
 
 import '../src/utils/set-css/viewportSize';
-import app from '../src/app';
 
 import LayoutHead from '../src/components/layout/head/Head';
 import Header from '../src/components/layout/header/Header';
@@ -25,7 +24,3 @@ function MyApp ({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
-
-if (app) {
-    app.html.classList.add('use-native-scroll');
-}
