5fbee1c58bbab9a419729048e18504534b29ef1a
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-09-02 14:31:43 +0300

    Update text-page

--- src/templates/text-page/index.tsx
+++ src/templates/text-page/index.tsx
@@ -3,6 +3,7 @@ import { TemplateBaseData } from '../_base/types';
 import AppPage from '../../app/AppPage';
 import LayoutSmoothScroll from '../../components/layout/smooth-scroll/LayoutSmoothScroll';
 import TextContent from '../../components/layout/text-content/TextContent';
+import styles from './Styles.module.scss';
 
 
 export interface TextPageTemplateData extends TemplateBaseData {
@@ -29,8 +30,10 @@ const TextPageTemplate = (
         <LayoutSmoothScroll>
             <div className="page-content">
                 <div className="wrap v-view_b">
-                    <TextContent html={content} />
-                    <div className="clear" />
+                    <div className={styles.text_page__content}>
+                        <TextContent html={content} />
+                        <div className="clear" />
+                    </div>
                 </div>
             </div>
         </LayoutSmoothScroll>
