86a2e4b39326fec476574b291c988ed435e43ebe
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-09-07 21:41:23 +0300

    Update app settings

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import isBrowser from './utils/browser/isBrowser';
 const app = isBrowser ? new Application<AppPage>() : false;
 export default app;
 
-// App Settings
+// Scroll Settings
 const useSmoothScroll = !(!!app && app.isMobile);
 const useWindowScroll = !useSmoothScroll;
 export const appSettings = {
@@ -13,13 +13,20 @@ export const appSettings = {
     useWindowScroll,
     useAdaptiveFontSize: true,
 };
-
-// fix scroll
-if (appSettings.useWindowScroll && isBrowser) {
+// set scroll classes
+if (app) {
+    if (useWindowScroll) {
+        app.html.classList.add('use-native-scroll');
+    } else if (useSmoothScroll) {
+        app.html.classList.add('use-smooth-scroll');
+    }
+}
+// disable scroll restoration
+if (isBrowser) {
     window.history.scrollRestoration = 'manual';
 }
-
 // scroll to top on resize (mobile bug)
+// when using custom scroll
 if (!!app && !useWindowScroll) {
     app.viewport.add('', () => {
         document.documentElement.scrollTop = 0;
@@ -28,13 +35,4 @@ if (!!app && !useWindowScroll) {
         timeout: 650,
         name: 'iOS scroll resize bug',
     });
-}
-
-// set scroll classes
-if (app) {
-    if (useWindowScroll) {
-        app.html.classList.add('use-native-scroll');
-    } else if (useSmoothScroll) {
-        app.html.classList.add('use-smooth-scroll');
-    }
 }
