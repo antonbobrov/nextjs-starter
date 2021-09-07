4eda4fd5a7b659bd117f2867b31968d501a30c0f
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-09-07 20:41:15 +0300

    Add HorizontalScrollList

---
+++ src/components/content/horizontal-scroll-list/index.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef } from 'react';
+import { ScrollBar, SmoothScroll } from 'vevet';
+import styles from './styles.module.scss';
+
+interface Data {
+    children: JSX.Element;
+}
+
+const HorizontalScrollList = ({
+    children,
+}: Data) => {
+    const ref = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        if (!ref.current) {
+            return () => {};
+        }
+        // create scroll list
+        const scroll = new SmoothScroll({
+            selectors: {
+                outer: ref.current,
+            },
+            isHorizontal: true,
+            overscroll: false,
+            useWheel: false,
+        });
+        // add scroll bar
+        const scrollbar = new ScrollBar({
+            container: scroll,
+        });
+        // destroy the scene
+        return () => {
+            scrollbar.destroy();
+            scroll.destroy();
+        };
+    }, [ref]);
+
+    return (
+        <div className={`${styles.container} v-smooth-scroll`} ref={ref}>
+            <div className="v-smooth-scroll__container">
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default HorizontalScrollList;
