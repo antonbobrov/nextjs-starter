221b9d19c708ecac42c2047b00b8095452fff27d
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-09-22 19:01:02 +0300

    Update nextjs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+const withTM = require('next-transpile-modules')(['vevet', 'vevet-dom'], {
+    resolveSymlinks: true,
+    debug: true,
+});
+
+module.exports = withTM({
     reactStrictMode: true,
     serverRuntimeConfig: {
         PROJECT_ROOT: __dirname,
     },
     trailingSlash: true,
-};
+});
