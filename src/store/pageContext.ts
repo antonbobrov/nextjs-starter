7c4e910fc078aecd3877c7e05678fe902ee5696b
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-10-27 15:45:55 +0300

    Fix

--- src/store/pageContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-import { BaseTemplateData } from '../types/page';
-
-const PageContext = createContext<BaseTemplateData>(undefined as any);
-export default PageContext;
