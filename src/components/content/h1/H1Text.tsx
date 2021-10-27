76386fc9266d7d14c8e4579f01f41f7f8aafc8dc
Anthony Bobrov <anton.bobrov@hotmail.com>
2021-10-27 15:53:49 +0300

    Fix

--- src/components/content/h1/H1Text.tsx
+++ src/components/content/h1/H1Text.tsx
@@ -14,6 +14,6 @@ const H1Text = () => {
     if (pagetitle) {
         return <span dangerouslySetInnerHTML={{ __html: pagetitle }} />;
     }
-    return '';
+    return <></>;
 };
 export default H1Text;
