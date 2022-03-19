522b4339cb9a4fcd9cb3bbd1a7685cd333507474
Anthony Bobrov <anton.bobrov@hotmail.com>
2022-03-19 16:25:52 +0300

    Update smooth scroll component

--- src/components/layout/smooth-scroll/index.tsx
+++ src/components/layout/smooth-scroll/index.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
+import styles from './styles.module.scss';
 
 const LayoutSmoothScroll: FC = ({
     children,
 }) => (
     <div
-        className="v-smooth-scroll"
+        className={`${styles.smooth_scroll} v-smooth-scroll`}
         id="smooth-scroll"
     >
         <div className="v-smooth-scroll__container">
