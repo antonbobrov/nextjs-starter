7b26920c645be31eca5baccfcb0f6cd6102e89a8
Anthony Bobrov <anton.bobrov@hotmail.com>
2022-05-09 14:16:06 +0300

    Add useOnPageBeforeShownHook hook

--- src/utils/hooks/vevet.ts
+++ src/utils/hooks/vevet.ts
@@ -1,5 +1,6 @@
 import { DependencyList, useEffect } from 'react';
 import app from 'src/app';
+import PCancelable from 'p-cancelable';
 
 export function useOnPageLoadedHook (
     effect: () => (void | (() => void)),
@@ -75,6 +76,47 @@ export function useOnPageShownHook (
             return () => {
                 destroyed = true;
                 promise.cancel();
+                if (unsubscribe) {
+                    unsubscribe();
+                }
+            };
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        deps,
+    );
+}
+
+export function useOnPageBeforeShownHook (
+    effect: () => (void | (() => void)),
+    deps?: DependencyList,
+) {
+    useEffect(
+        () => {
+            let destroyed = false;
+            let unsubscribe: (() => void) | undefined | void;
+            const promises: PCancelable<any>[] = [];
+
+            const onPageCreated = app.onPageCreated();
+            promises.push(onPageCreated);
+            onPageCreated.then((page) => {
+                if (destroyed) {
+                    return;
+                }
+                const onBeforeShow = page.onBeforeShow();
+                promises.push(onBeforeShow);
+                onBeforeShow.then(() => {
+                    if (destroyed) {
+                        return;
+                    }
+                    unsubscribe = effect();
+                }).catch(() => {});
+            }).catch(() => {});
+
+            return () => {
+                destroyed = true;
+                promises.forEach((promise) => {
+                    promise.cancel();
+                });
                 if (unsubscribe) {
                     unsubscribe();
                 }
